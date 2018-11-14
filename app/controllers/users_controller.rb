e6feb6361a904b2503cdee089587cf661ee37168
Daniel <d.t.nguyen@live.com>
2018-11-14 03:27:24 +0000

    fixed login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def new
   end
-  
+
   def create
     user = User.new(user_params)
     if user.save
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
     end
   end
 
-  private
+private
 
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
