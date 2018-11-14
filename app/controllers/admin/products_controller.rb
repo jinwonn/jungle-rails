d461f995aad68aac84377454d12f43f5d6ff147f
Daniel <d.t.nguyen@live.com>
2018-11-14 01:40:50 +0000

    admin auth for admin pages added

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,7 @@
 class Admin::ProductsController < ApplicationController
 
+  http_basic_authenticate_with name: ENV["WEB_USERNAME"], password: ENV["WEB_PASSWORD"]
+
   def index
     @products = Product.order(id: :desc).all
   end
